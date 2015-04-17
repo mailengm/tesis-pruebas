a4f44687ec0a44af2797153e52b7512ff8dbca8f
andresrabinovich@gmail.com <andresrabinovich@gmail.com>
2015-04-17 16:30:30 -0300

    Hago el tutorial 2.a de WGCNA, Automatic, one-step network construction and module detection

--- WGCNA/tutoriales R/tutorial_2.R
+++ WGCNA/tutoriales R/tutorial_2.R
@@ -1,18 +1,58 @@
-# Display the current working directory
-initial.dir<-getwd();
-# If necessary, change the path below to the directory where the data files are stored.
-# "." means current directory. On Windows use a forward slash / instead of the usual \.
-workingDir = "FemaleLiver-Data/";
-setwd(workingDir);
 # Load the WGCNA package
 library(WGCNA);
 # The following setting is important, do not omit.
 options(stringsAsFactors = FALSE);
+
+ruta="../Escritorio/tesis-pruebas/WGCNA/tutoriales R/";
+
 #Read in the female liver data set
-enableWGCNAThreads()
-lnames = load(file = "FemaleLiver-01-dataInput.RData");
+#enableWGCNAThreads()
+lnames = load(file = paste(ruta, "FemaleLiver-Data/FemaleLiver-01-dataInput.RData", sep=""));
+
+# Choose a set of soft-thresholding powers
+powers = c(c(1:10), seq(from = 12, to=20, by=2))
+# Call the network topology analysis function
+sft = pickSoftThreshold(datExpr, powerVector = powers, verbose = 5)
+sizeGrWindow(9, 5)
+par(mfrow = c(1,2));
+cex1 = 0.9;
+# Scale-free topology fit index as a function of the soft-thresholding power
+plot(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
+xlab="Soft Threshold (power)",ylab="Scale Free Topology Model Fit,signed R^2",type="n",
+main = paste("Scale independence"));
+text(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
+labels=powers,cex=cex1,col="red");
+
+# this line corresponds to using an R^2 cut-off of h
+abline(h=0.90,col="red")
+# Mean connectivity as a function of the soft-thresholding power
+plot(sft$fitIndices[,1], sft$fitIndices[,5],
+xlab="Soft Threshold (power)",ylab="Mean Connectivity", type="n",
+main = paste("Mean connectivity"))
+text(sft$fitIndices[,1], sft$fitIndices[,5], labels=powers, cex=cex1,col="red")
+
+net = blockwiseModules(datExpr, power = 6,
+TOMType = "unsigned", minModuleSize = 30,
+reassignThreshold = 0, mergeCutHeight = 0.25,
+numericLabels = TRUE, pamRespectsDendro = FALSE,
+saveTOMs = TRUE,
+saveTOMFileBase = "femaleMouseTOM",
+verbose = 3)
+
+# open a graphics window
+sizeGrWindow(12, 9)
+# Convert labels to colors for plotting
+mergedColors = labels2colors(net$colors)
+# Plot the dendrogram and the module colors underneath
+plotDendroAndColors(net$dendrograms[[1]], mergedColors[net$blockGenes[[1]]],
+"Module colors",
+dendroLabels = FALSE, hang = 0.03,
+addGuide = TRUE, guideHang = 0.05)
 
 
-
-
-setwd(initial.dir);
+moduleLabels = net$colors
+moduleColors = labels2colors(net$colors)
+MEs = net$MEs;
+geneTree = net$dendrograms[[1]];
+save(MEs, moduleLabels, moduleColors, geneTree,
+file = "FemaleLiver-02-networkConstruction-auto.RData")
