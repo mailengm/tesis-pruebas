441e88f206e72e3990461bca67bf0aac7c85bdae
mailengm <mailen.gomezmayol@gmail.com>
2015-05-21 00:51:09 -0300

    Update barrido_nstart_maxiter_dendro-2.R
    Entendiendo dynamic tree cut

--- WGCNA/pruebas/barrido_nstart_maxiter_dendro-2.R
+++ WGCNA/pruebas/barrido_nstart_maxiter_dendro-2.R
@@ -1,5 +1,5 @@
 #TO DO
-#probar otros métodos en clvalid
+#probar otros mÃ©todos en clvalid
 
 library(fields);
 
@@ -11,7 +11,7 @@ ancho_del_cluster = 0.25; #lo que mide el cluster en x
 alto_del_cluster = 0.25; #lo que mide el cluster en y
 iteraciones_de_k = 50; #cuantas veces vamos a probar ajustar con cada k el mismo set de datos
 maximas_iteraciones_de_k_means = 50; #Maximo de iteraciones que hace el kmeans si no converge
-nivel_de_ruido = 0.5; #entre 0 y 1, es el porcentaje de ruido en función de la cantidad de puntos que haya
+nivel_de_ruido = 0.5; #entre 0 y 1, es el porcentaje de ruido en funciÃ³n de la cantidad de puntos que haya
 saltos_de_ruido = 10;
 saltos_de_dispersion = 10;
 cantidad_de_k_a_probar = 20; #Vamos a probar los k entre dim_red^2-cantidad_de_k_a_probar/2 y dim_red^2+cantidad_de_k_a_probar/2
@@ -21,7 +21,7 @@ guardar_datos_de_corrida = FALSE;
 #limpiamos los graficos actuales
 graphics.off();
 
-#creo un directorio para guardar ahí todos los datos de la corrida
+#creo un directorio para guardar ahÃ­ todos los datos de la corrida
 if(guardar_datos_de_corrida){
 	cat("Se van a guardar los datos de la corrida\n")
 	flush.console();
@@ -30,7 +30,7 @@ if(guardar_datos_de_corrida){
 	dir.create(script.directorio_de_archivos);
 }
 
-#Traemos la librería de cluster para usar silhouette y otras cosas
+#Traemos la librerÃ­a de cluster para usar silhouette y otras cosas
 library(cluster);
 
 #genero la red equiespaciada
@@ -45,12 +45,12 @@ total_de_puntos <- puntos_en_la_red * puntos_por_cluster;
 #Genero los puntos de ruido que son la misma cantidad que los puntos de la red pero no clusterizados. A medida que aumento el nivel de ruido los voy agregando
 #puntos_de_ruido <- matrix(runif(total_de_puntos*nivel_de_ruido*2, parametro_de_red-ancho_del_cluster, parametro_de_red*dim_red+ancho_del_cluster), nrow=total_de_puntos*nivel_de_ruido, ncol=2);
 
-#graficó los puntos y la red
+#graficÃ³ los puntos y la red
 #plot(puntos, main="Red a clusterizar", xlab="x",ylab="y");
 #points(red, col="yellow");
 #points(puntos_de_ruido, col="red");
 #legend((dim_red*parametro_de_red-4), (dim_red*parametro_de_red-1), c("Puntos", "Centros de cluster", "Ruido"), pch=c(1,1, 1), col=c("black", "yellow", "red"))
-if(guardar_datos_de_corrida) savePlot(filename = paste(script.directorio_de_archivos, "/red.png", sep=""), "png");#guardo el plot en un archivo además de mostrarlo en pantalla
+if(guardar_datos_de_corrida) savePlot(filename = paste(script.directorio_de_archivos, "/red.png", sep=""), "png");#guardo el plot en un archivo ademÃ¡s de mostrarlo en pantalla
 
 
 
@@ -60,7 +60,7 @@ semi_cantidad_de_k_a_probar = cantidad_de_k_a_probar/2;
 
 #Probamos kmeans para varios k, desde puntos_en_la_red-10 hasta puntos_en_la_red+10, 
 #nos quedamos con el de mejor promedio, iteramos varias veces y graficamos 
-#un histograma para ver que k ajustó mejor la mayor cantidad de veces
+#un histograma para ver que k ajustÃ³ mejor la mayor cantidad de veces
 mejor_k <- matrix(0, nrow=10, ncol=10); #Vamos a guardar el mejor k y el promedio que dio
 mejor_p <- matrix(0, nrow=10, ncol=10); #Vamos a guardar el mejor k y el promedio que dio
 promedios <- matrix(0, nrow=cantidad_de_k_a_probar, ncol=2);
@@ -74,11 +74,11 @@ tiempo_de_inicio <- proc.time();
 print("Comenzando corrida");
 flush.console();
 
-#Agrego una lista para guardar los valores de silhouette de cada iteraci�n en k
+#Agrego una lista para guardar los valores de silhouette de cada iteración en k
 lsil<-list();
 data<-list();
 ldispersion<-list();
-#Bandera que me indica si ya mostró el estimado de tiempo que va a demorar la corrida
+#Bandera que me indica si ya mostrÃ³ el estimado de tiempo que va a demorar la corrida
 ya_mostro_tiempo_estimado = FALSE
 
 
@@ -103,10 +103,36 @@ for(j in saltos_de_dispersion:1){
   #  for(k in 2:(cantidad_de_k_a_probar+1)){
       #calculo los kmeans con k puntos aleatorios, el silhouette
       #y saco un promedio de los promedios de cada cluster
-      #en el silhouette como medida de cuan buena fue la clusterización para ese k
+      #en el silhouette como medida de cuan buena fue la clusterizaciÃ³n para ese k
       #km<-kmeans(puntos, k, iter.max = maximas_iteraciones_de_k_means, nstart = iteraciones_de_k);
       tree=hclust(d, method="average");
       clus=cutree(tree,k);
+      
+      if(FALSE){
+      #Dynamic Tree Cut. 
+      #Primero corto por lm
+      lm=mean(tree$height)
+      #Despues por ld
+      ld=(lm+min(tree$height))*0.5
+      #finalmente por lu
+      lu=(lm+max(tree$height))*0.5
+      
+      ############################
+      #quiero el plot de las alturas.fig 1 like.
+      permutaciones=tree$order;
+      alturas=tree$height[permutaciones] #esto no me ordena todo bien :(
+      corte= lm; #?
+      plot(alturas,typ="h")
+      abline(h=corte,col="red")
+      
+      ############################
+      #Para plotear los puntos
+      
+       plot(puntos[,1],puntos[,2])
+       points(puntos[,1],puntos[,2],col=dynamic+1,pch=20) #cambiar dynamic por lo correspondiente
+
+      }
+      
       
       #data[[as.character(j,i)]]<-puntos
       #s<-dunn(d, km$cluster);
@@ -118,7 +144,7 @@ for(j in saltos_de_dispersion:1){
   #  }
     
    dynamic=cutreeDynamic(dendro=tree,distM=as.matrix(d),method="hybrid",pamStage=FALSE,
-                         minClusterSize=4,maxCoreScatter=0.04)
+                         minClusterSize=4,maxCoreScatter=0.04)#los clusters dependen mucho de estos parámetros. :(
 
    if(FALSE){
     plot(puntos[,1],puntos[,2],typ="n")
@@ -132,23 +158,23 @@ for(j in saltos_de_dispersion:1){
     #Meto los puntos de ruido entre los puntos de los clusters
     puntos <- rbind(puntos, matrix(runif(total_de_puntos*2*nivel_de_ruido, parametro_de_red-ancho_del_cluster, parametro_de_red*dim_red+ancho_del_cluster), ncol=2));
     
-    # Usando distancias ultram�tricas:
+    # Usando distancias ultramétricas:
     
     
     #lruido[[i]]<-s<-silhouette(clus,d);
     #si<-summary(s);
     #media[i,j]=si$si.summary["Mean"];
-    #El que dio el promedio más alto es el mejor k según este criterio
+    #El que dio el promedio mÃ¡s alto es el mejor k segÃºn este criterio
     #mejor_k[j,i] <- promedios[which.max(promedios[,1]), 2];
     #mejor_p[j,i] <- promedios[which.max(promedios[,1]), 1];
-    #Muestro cuanto tardó en correr uno y tiro un estimado de cuanto va a demorar todo
+    #Muestro cuanto tardÃ³ en correr uno y tiro un estimado de cuanto va a demorar todo
     mejor_p[j,i]<-si$si.summary["Mean"]
  mejor_k[j,i] <- mejor_k<-nrow(table(cutreeDynamic(tree)));
   }
   
   ldispersion[[j]]<-lruido
 
- #El que dio el promedio más alto es el mejor k según este criterio
+ #El que dio el promedio mÃ¡s alto es el mejor k segÃºn este criterio
  #mejor_k[j,i] <- promedios[which.max(promedios[,1]), 2];
  #mejor_p[j,i] <- promedios[which.max(promedios[,1]), 1];
  
@@ -184,7 +210,7 @@ for(j in saltos_de_dispersion:1){
    #cat("Tiempo estimado de corrida: ", round(cantidad_de_k_a_probar * tiempo_para_1_iteracion["elapsed"]), " segundos\n", sep="")
     ya_mostro_tiempo_estimado = TRUE;
   }
-  #Imprimo la iteración actual para saber por donde va y cuanto falta
+  #Imprimo la iteraciÃ³n actual para saber por donde va y cuanto falta
   print(j);
   flush.console();
   
@@ -193,15 +219,15 @@ for(j in saltos_de_dispersion:1){
 
 
 
-#Mostramos un histograma para ver cual es el k que mejor ajustó
+#Mostramos un histograma para ver cual es el k que mejor ajustÃ³
 #dev.new();
 #hist(mejor_k[,2], (puntos_en_la_red-10):(puntos_en_la_red+10), main="Histograma de mejores k", xlab="k",ylab="Ocurrencias")
-if(guardar_datos_de_corrida) savePlot(filename = paste(script.directorio_de_archivos, "/histograma.png", sep=""), "png");#guardo el plot en un archivo además de mostrarlo en pantalla
+if(guardar_datos_de_corrida) savePlot(filename = paste(script.directorio_de_archivos, "/histograma.png", sep=""), "png");#guardo el plot en un archivo ademÃ¡s de mostrarlo en pantalla
 
 #Mostramos los promedios para los mejores k para ver entre que valores se mueven
 #dev.new();
-#plot(mejor_k[,1], main="Valores de los mejores k en cada iteracion", xlab="N° de iteracion", ylab="Promedio")
-if(guardar_datos_de_corrida) savePlot(filename = paste(script.directorio_de_archivos, "/promedios.png", sep=""), "png");#guardo el plot en un archivo además de mostrarlo en pantalla
+#plot(mejor_k[,1], main="Valores de los mejores k en cada iteracion", xlab="NÂ° de iteracion", ylab="Promedio")
+if(guardar_datos_de_corrida) savePlot(filename = paste(script.directorio_de_archivos, "/promedios.png", sep=""), "png");#guardo el plot en un archivo ademÃ¡s de mostrarlo en pantalla
 
 #Guardo los datos de la corrida en un archivo de R
 if(guardar_datos_de_corrida) save(red, puntos, mejor_k, dim_red, puntos_por_cluster, parametro_de_red, ancho_del_cluster, alto_del_cluster, iteraciones_de_k, nivel_de_ruido, file = paste(script.directorio_de_archivos, "/kmeans.RData", sep=""));
